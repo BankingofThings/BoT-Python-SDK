f44b3d11d0dc6783c27db27cd1c335e712d1d460
Coen Mooij <coenmooij@gmail.com>
2018-08-22 13:07:05 +0200

    fix test and add pytest to requirements

--- tests/test_key_generator.py
+++ tests/test_key_generator.py
@@ -3,4 +3,4 @@ from bot_python_sdk.key_generator import KeyGenerator
 
 def test_generate_uuid():
     uuid = KeyGenerator().generate_uuid()
-    assert isinstance(s, str)
+    assert len(str(uuid)) == 36
