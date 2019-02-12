7307435a07a9f6e692f2bb6e81ee6b5048576d8a
ShanavazJs <tech.shanu@gmail.com>
2019-02-12 17:39:32 +0530

    worked for change request

--- bot_python_sdk/store.py
+++ bot_python_sdk/store.py
@@ -8,83 +8,89 @@ _qr_image_path = 'storage/qr.png'
 _configuration_file_path = 'storage/configuration.json'
 _bot_public_key = 'storage/public.pem'
 
-
 LOCATION = 'Store'
 
 
 class Store:
+    def __init__(self):
+        pass
+
     @staticmethod
     def set_actions(actions):
         try:
-            with open(_actions_file_path, 'w') as outfile:
-                outfile..write(json.dumps(actions))
-        except IOError:
-            Logger.error(LOCATION, 'Unable to write in '+_actions_file_path+' path')
+            with open(_actions_file_path, 'w') as file:
+                file.write(json.dumps(actions))
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to write in {0} path'.format(_actions_file_path))
+            raise io_error.message
 
     @staticmethod
     def get_actions():
         if not os.path.isfile(_actions_file_path):
             return []
         try:
-            with open(_actions_file_path, 'r') as outfile:
-                actions = json.loads(outfile.read())
+            with open(_actions_file_path, 'r') as file:
+                actions = json.loads(file.read())
                 return actions
-        except IOError:
-            Logger.error(LOCATION, 'Unable to read in '+_actions_file_path+' path')
-            return None
-
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to read in {0} path'.format(_actions_file_path))
+            raise io_error.message
 
     @staticmethod
     def get_last_triggered(action_id):
         if not os.path.isfile(_last_triggered_file_path):
             return None
         try:
-            with open(_last_triggered_file_path) as outfile:
-                data = json.loads(outfile.read())
+            with open(_last_triggered_file_path) as file:
+                data = json.loads(file.read())
                 return data[action_id] if action_id in data.keys() else None
-        except IOError:
-            Logger.error(LOCATION, 'Unable to read '+_last_triggered_file_path+' path')
-            return None
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to read {0} path'.format(_last_triggered_file_path))
+            raise io_error.message
 
     @staticmethod
     def set_last_triggered(action_id, time):
         data = {}
         if os.path.isfile(_last_triggered_file_path):
-            with open(_last_triggered_file_path, 'r') as outfile:
-                data = json.loads(outfile.read())
+            with open(_last_triggered_file_path, 'r') as file:
+                data = json.loads(file.read())
         data[action_id] = time
         try:
-            with open(_last_triggered_file_path, 'w') as outfile:
-                outfile.write(json.dumps(data))
-        except IOError:
-            Logger.error(LOCATION, 'Unable to write '+_last_triggered_file_path+' path')
+            with open(_last_triggered_file_path, 'w') as file:
+                file.write(json.dumps(data))
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to write {0} path'.format(_last_triggered_file_path))
+            raise io_error.message
 
     @staticmethod
     def save_qrcode(image):
         try:
-            with open(_qr_image_path, 'wb') as outfile:
-                image.save(outfile)
-        except IOError:
-            Logger.error(LOCATION, 'Unable to save image in '+_qr_image_path+' path')
-
+            with open(_qr_image_path, 'wb') as file:
+                image.save(file)
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to save image in {0} path'.format(_qr_image_path))
+            raise io_error.message
 
     @staticmethod
     def get_configuration():
         try:
-            with open(_configuration_file_path, 'r') as outfile:
-                configuration = outfile.read()
+            with open(_configuration_file_path, 'r') as file:
+                configuration = file.read()
                 return json.loads(configuration)
-        except IOError:
-            Logger.error(LOCATION, 'Unable to read in '+_configuration_file_path+' path')
-            return None
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to read in {0} path'.format(_configuration_file_path))
+            raise io_error.message
 
     @staticmethod
     def set_configuration(configuration):
         try:
-            with open(_configuration_file_path, 'w') as outfile:
-                 outfile.write(json.dumps(configuration))
-        except IOError:
-            Logger.error(LOCATION, 'Unable to write '+_configuration_file_path+' path')
+            with open(_configuration_file_path, 'w') as file:
+                file.write(json.dumps(configuration))
+        except IOError as io_error:
+            Logger.error(LOCATION, 'Unable to write {0} path'.format(_configuration_file_path))
+            raise io_error.message
+
+
 
     @staticmethod
     def has_configuration():
@@ -93,9 +99,8 @@ class Store:
     @staticmethod
     def get_bot_public_key():
         try:
-            with open(_bot_public_key) as outfile:
-                public_key = outfile.read()
+            with open(_bot_public_key) as file:
+                public_key = file.read()
                 return public_key
         except IOError:
-            Logger.error(LOCATION, 'Unable to read in '+_bot_public_key+' path')
-            return None
+            Logger.error(LOCATION, 'Unable to read in {0} path'.format(_bot_public_key))
