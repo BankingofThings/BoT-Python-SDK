6c7547b7cf0cd31f40104f09e42beb89e67a8725
ercanbozogluing <ercanbozogluing@users.noreply.github.com>
2020-11-04 12:29:55 +0100

    pair loop created

--- bot_python_sdk/Utils.py
+++ bot_python_sdk/Utils.py
@@ -1,12 +1,10 @@
 import re
-import subprocess
+
 
 class Utils:
     @staticmethod
     # Function to validate the given IP Address
     def is_valid(ip):
-        return re.search('''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)''', ip)
-
-    @staticmethod
-    def getIpAddress():
-        return subprocess.Popen(['hostname', '-I'], stdout=subprocess.PIPE).communicate()[0].decode('ascii').split(' ')[0]
+        return re.search(
+            '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)''',
+            ip)
--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import sys
 
+from waitress import serve
+
 from bot_python_sdk.Utils import Utils
 from bot_python_sdk.configuration_service import ConfigurationService
 from bot_python_sdk.store import Store
@@ -12,36 +14,38 @@ store = Store()
 
 # If OS is windows based, it doesn't support gunicorn so we run waitress
 # TODO doesn't work with pyCharm
-def start_server():
-    ip = Utils.getIpAddress()
-    Logger.info('Server', "starting with configuration... IP" + ip)
-    if Utils.is_valid(ip):
-        Logger.info('Server', "Detected IP Address :" + ip)
-    else:
-        Logger.info('Server', "Failed in detecting valid IP Address, using loop back address: 127.0.0.1")
-        ip = '127.0.0.1'
+def get_ip():
+    ip_address = subprocess.Popen(['hostname', '-I'], stdout=subprocess.PIPE).communicate()[0].decode('ascii').split(' ')[0]
 
-    Logger.info('Server', "Starting Webserver at URL: http://" + ip + ':3001/')
-    subprocess.run(['gunicorn', '-b', ip + ':3001', 'bot_python_sdk.api:api'])
+    Logger.info('Server', "starting with configuration... IP" + ip_address)
+
+    if Utils.is_valid(ip_address):
+        Logger.info('Server', "Detected IP Address :" + ip_address)
+    else:
+        ip_address = '127.0.0.1'
+        Logger.info('Server', "Failed in detecting valid IP Address, using loop back address: " + ip_address)
+
+    return ip_address
+
+
+def start_server():
+    ip_address = get_ip()
+
+    Logger.info('Server', "Starting Webserver at URL: http://" + ip_address + ':3001/')
+    subprocess.run(['gunicorn', '-b', ip_address + ':3001', 'bot_python_sdk.api:api'])
     Logger.info('Server', 'Webserver is running')
 
 
-if os.name == 'nt':
-    ip = Utils.getIpAddress()
-    Logger.info('Server', "starting with configuration... IP" + ip)
+if not store.has_configuration():
+    if len(sys.argv) != 2:
+        exit('Please add your productID to configure the SDK: "make server productID=YOUR_PRODUCT_ID"')
+    elif len(sys.argv[1]) != 36:
+        exit('Please enter a valid productID')
+    else:
+        # argv is the console input
+        productID = sys.argv[1]
 
-    subprocess.run(['waitress-serve', '--port=3001', 'bot_python_sdk.api:api'])
-else:
-    if not store.has_configuration():
-        if len(sys.argv) != 2:
-            exit('Please add your productID to configure the SDK: "make server productID=YOUR_PRODUCT_ID"')
-        elif len(sys.argv[1]) != 36:
-            exit('Please enter a valid productID')
-        else:
-            # argv is the console input
-            productID = sys.argv[1]
+        Logger.info('Server', "starting with configuration. ProductID " + productID)
+        ConfigurationService().initialize_configuration(productID)
 
-            Logger.info('Server', "starting with configuration. ProductID " + productID)
-            ConfigurationService().initialize_configuration(productID)
-
-    start_server()
+start_server()
