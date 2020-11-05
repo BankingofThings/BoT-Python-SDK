999ab40e536f0704f43dc236512d2386133bd255
ercanbozogluing <ercanbozogluing@users.noreply.github.com>
2020-11-05 20:49:32 +0100

    Revert "to dev"
    This reverts commit 9aa3e172

--- bot_python_sdk/finn.py
+++ bot_python_sdk/finn.py
@@ -44,13 +44,9 @@ class Finn:
         if not __store.has_configuration():
             Logger.info('Finn', '__init__' + str(sys.argv))
             if len(sys.argv) != 2:
-                Logger.info('Finn', '__init__ 1')
-                id = input('Please add your productID to configure the SDK: "make server productID=YOUR_PRODUCT_ID"')
-                Logger.info('Finn', '__init__' + ' id = ' + id)
+                exit('Please add your productID to configure the SDK: "make server productID=YOUR_PRODUCT_ID"')
             elif len(sys.argv[1]) != 36:
-                id = Logger.info('Finn', '__init__ 2')
-                Logger.info('Finn', '__init__' + ' id = ' + id)
-                input('Please enter a valid productID')
+                exit('Please enter a valid productID')
             else:
                 # argv is the console input
                 self.__productID = sys.argv[1]
