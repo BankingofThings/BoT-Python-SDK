f41c8c870cba208a2c9ca7de586fe9d6a817c8f7
ercanbozogluing <ercanbozogluing@users.noreply.github.com>
2020-11-05 15:03:43 +0100

    Revert "to dev"
    This reverts commit 49c72bbc

--- server.py
+++ server.py
@@ -3,9 +3,6 @@ import os
 import signal
 import subprocess
 
-import falcon
-
-from bot_python_sdk.finn import Finn
 from bot_python_sdk.utils import Utils
 
 from bot_python_sdk.logger import Logger
@@ -26,15 +23,13 @@ else:
 
 Logger.info('Server', "Starting server at URL: http://" + __ip_address + ':3001/')
 
-api = application = falcon.API()
-Finn(api)
-
 # Executes api.py and indirectly finn.py
-# process = subprocess.Popen(['gunicorn', '-b', __ip_address + ':3001', 'bot_python_sdk.api:api'])
+process = subprocess.Popen(['gunicorn', '-b', __ip_address + ':3001', 'bot_python_sdk.api:api'])
 
-# def __stop_gunicorn():
-#     Logger.info('Server', '__stop_gunicorn')
-#     os.kill(process.pid, signal.SIGTERM)
-#
-#
-# atexit.register(__stop_gunicorn)
+
+def __stop_gunicorn():
+    Logger.info('Server', '__stop_gunicorn')
+    os.kill(process.pid, signal.SIGTERM)
+
+
+atexit.register(__stop_gunicorn)
