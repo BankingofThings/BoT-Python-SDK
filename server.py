5498e5e69cd302311d97ccdaa34f456a032a9629
ercanbozogluing <ercanbozogluing@users.noreply.github.com>
2020-11-05 12:43:15 +0100

    to dev

--- server.py
+++ server.py
@@ -30,11 +30,11 @@ Logger.info('Server', "Starting server at URL: http://" + __ip_address + ':3001/
 # Finn(api)
 
 # Executes api.py and indirectly finn.py
-process = subprocess.Popen(['gunicorn', '-b', __ip_address + ':3001', 'bot_python_sdk.api:api'])
-
-def __stop_gunicorn():
-    Logger.info('Server', '__stop_gunicorn')
-    os.kill(process.pid, signal.SIGTERM)
-
-
-atexit.register(__stop_gunicorn)
+subprocess.run(['gunicorn', '-b', __ip_address + ':3001', 'bot_python_sdk.api:api'])
+#
+# def __stop_gunicorn():
+#     Logger.info('Server', '__stop_gunicorn')
+#     os.kill(process.pid, signal.SIGTERM)
+#
+#
+# atexit.register(__stop_gunicorn)
