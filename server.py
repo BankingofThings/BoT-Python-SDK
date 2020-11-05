49c72bbc70083b7f18b887187c2ca5c4a54b53f8
ercanbozogluing <ercanbozogluing@users.noreply.github.com>
2020-11-05 12:35:46 +0100

    to dev

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import os
 import signal
 import subprocess
 
+import falcon
+
+from bot_python_sdk.finn import Finn
 from bot_python_sdk.utils import Utils
 
 from bot_python_sdk.logger import Logger
@@ -23,13 +26,15 @@ else:
 
 Logger.info('Server', "Starting server at URL: http://" + __ip_address + ':3001/')
 
+api = application = falcon.API()
+Finn(api)
+
 # Executes api.py and indirectly finn.py
-process = subprocess.Popen(['gunicorn', '-b', __ip_address + ':3001', 'bot_python_sdk.api:api'])
+# process = subprocess.Popen(['gunicorn', '-b', __ip_address + ':3001', 'bot_python_sdk.api:api'])
 
-
-def __stop_gunicorn():
-    Logger.info('Server', '__stop_gunicorn')
-    os.kill(process.pid, signal.SIGTERM)
-
-
-atexit.register(__stop_gunicorn)
+# def __stop_gunicorn():
+#     Logger.info('Server', '__stop_gunicorn')
+#     os.kill(process.pid, signal.SIGTERM)
+#
+#
+# atexit.register(__stop_gunicorn)
