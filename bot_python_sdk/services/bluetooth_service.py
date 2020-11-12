4a863672945dd4d19b619e3424de931895af44ba
ercanbozogluing <ercanbozogluing@users.noreply.github.com>
2020-11-12 13:53:07 +0100

    Revert "to dev"
    This reverts commit 9aa3e172

--- bot_python_sdk/services/bluetooth_service.py
+++ bot_python_sdk/services/bluetooth_service.py
@@ -21,6 +21,8 @@ class BluetoothService:
         self.__bleno = Bleno()
         self.__bleno.onAdvertisingStart(self.on_advertising_start)
         self.__bleno.onStateChange(self.on_state_change)
+        Logger.info('BluetoothService', '__init__' + str(self.__bleno))
+        self.__bleno.start()
 
     # start advertising depending on ble state (powered on/off)
     def start_advertising(self):
